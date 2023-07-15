d08b7e0cc3165c9bb113c06bea54d69ee8b73890
088ashu <53143203+088ashu@users.noreply.github.com>
2023-07-15 17:15:35 +0530

    Springboot-app-CRUD-Operation

---
+++ Springboot-app-CRUD-Operation/src/main/java/com/codewithdurgesh/blog/SpringbootAppCrudOperationApplication.java
@@ -0,0 +1,13 @@
+package com.codewithdurgesh.blog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootAppCrudOperationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootAppCrudOperationApplication.class, args);
+	}
+
+}
